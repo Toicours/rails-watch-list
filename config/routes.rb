6089d14d97a227214e9d6511c4150708b3005702
Mathieu Courtois <kourtois@protonmail.com>
2021-11-11 12:45:42 +0100

    setting up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "movies#home"
+  resources :movies do
+    member do
+      get :bookmarks
+    end
+  end
 end
